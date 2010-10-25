7dc61a5a75394cf8752223a7530209c982cd4f9c
lapis25 <lapis25@gmail.com>
2010-10-26 00:03:31 +0900

    initial commit

---
+++ bin/kansaipm-rt-bot.pl
@@ -0,0 +1,48 @@
+#!perl
+
+use strict;
+use warnings;
+
+use Config::Pit 'pit_get';
+use AnyEvent::Twitter;
+use AnyEvent::Twitter::Stream;
+
+binmode STDOUT, ":utf8";
+
+my $track = 'kansai.pm,kansaipm';
+
+my $config = pit_get('twitter.kansai.pm', require => {
+    consumer_key    => 'consumer key',
+    consumer_secret => 'consumer secret',
+    token           => 'token',
+    token_secret    => 'token secret',
+});
+
+my $cv = AnyEvent->condvar;
+
+my $twitty = AnyEvent::Twitter->new(%$config);
+my $streamer = AnyEvent::Twitter::Stream->new(
+    %$config,
+    method => 'filter',
+    track => $track,
+    on_tweet => sub {
+        my $tweet = shift;
+        print "$tweet->{created_at},$tweet->{user}{screen_name}: $tweet->{text} [$tweet->{id}]\n";
+        $twitty->request(
+            api    => "statuses/retweet/$tweet->{id}",
+            method => 'POST',
+            sub {
+            },
+        );
+    },
+    on_error => sub {
+        my $error = shift;
+        warn "ERROR: $error";
+        $cv->send;
+    },
+    on_eof   => sub {
+        $cv->send;
+    },
+);
+
+$cv->recv;
